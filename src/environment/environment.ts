f3076bae8b9095d41f0d2d88f418df0117e56444
ifcamac1 <ifcamac1@MacBook-Pro.local>
2018-06-26 14:38:53 +0700

    akmal

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -25,7 +25,8 @@ export const environment = {
   // Url_API:'http://192.168.1.69:2121/WaskitaAPI2/',
   // Url_API:'http://35.197.137.111/WaskitaAPI/',
   // Url_API:'http://35.197.137.111/WaskitaAPI2/',
-  Url_API:'http://35.197.137.111/Supermall_API/',
+  Url_API:'http://139.255.61.85/WaskitaAPI2/',
+  // Url_API:'http://35.197.137.111/Supermall_API/',
   cons_pb:'IFCAPB',
   cons_mobile:'IFCAMOBILE'
 };
